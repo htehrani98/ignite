77f88ec20b61aadaa5ddd74fbf536b05a80db09a
htehrani98 <h.tehrani1377@gmail.com>
2022-08-11 08:24:34 +0430

    3. Setting Up Redux

--- src/core/reducers/index.js
+++ src/core/reducers/index.js
@@ -2,6 +2,19 @@ import { combineReducers } from "redux";
 
 import { gamesReducer } from "./games-reducer";
 
+const initState = {
+  name: "",
+  isLogged: false,
+};
+
+const userReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
 export const rootReducer = combineReducers({
   games: gamesReducer,
+  user: userReducer,
 });
