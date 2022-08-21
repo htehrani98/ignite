e29f03b07e88294a2f9e613c9d5b7be9f68263d0
htehrani98 <h.tehrani1377@gmail.com>
2022-08-22 00:29:42 +0430

    8. Styling Up The Cards

--- src/core/components/game.js
+++ src/core/components/game.js
@@ -3,12 +3,24 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-export const Game = (name, released, image) => {
+export const Game = ({ name, released, image }) => {
   return (
-    <div>
+    <StyledGame>
       <h3>{name}</h3>
       <p>{released}</p>
       <img src={image} alt={name} />
-    </div>
+    </StyledGame>
   );
 };
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgb(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
+`;
--- src/pages/home.js
+++ src/pages/home.js
@@ -32,5 +32,16 @@ export const Home = () => {
   );
 };
 
-const GameList = styled(motion.div)``;
-const Games = styled(motion.div)``;
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
