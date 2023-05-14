b7b7ce6491e03c941b49595f049e29f5b8099d0d
htehrani98 <h.tehrani1377@gmail.com>
2023-05-15 00:00:48 +0430

    change api key

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import { getCurrentDay, getCurrentMonth } from './core/utils/date';
-// for example upComingGamesURL = https://api.rawg.io/api/games?dates=2022-08-21,2023-08-21&ordering=-added&page_size=16&key=0150979fafd046608086dec5c755559e
-
+// for https://api.rawg.io/api/games?dates=2022-08-21,2023-08-21&ordering=-added&page_size=16&key=c36666d4c9ee49c59ae67b516e4ab6bb
+// GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
+// GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
 const currentYear = new Date().getFullYear();
 const currentDay = getCurrentDay();
 const currentMonth = getCurrentMonth();
@@ -9,7 +10,7 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 export const createGameEndpoint = url => {
-  return `https://api.rawg.io/api/games${url}key=0150979fafd046608086dec5c755559e`;
+  return `https://api.rawg.io/api/games${url}key=c36666d4c9ee49c59ae67b516e4ab6bb`;
 };
 
 export const popularGamesURL = () =>
